397a1f0b838129ed8e4401f2015c559ac638c6bf
jaffa Isaac <jaffaisaac4@gmail.com>
2023-04-04 23:04:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// these are the different ways to write arrow functions in javascript
+// Arrow fuction is an Es6 way for short hand writting of the functions
+let func1 = ()=>ab;
+
+let func2 = a => b;
+
+let func3 =()=>{return good};
+
+let func4 =(a,b,c)=>{return solution};
+
+let funct5 =(a=5, b=2)=>a+b;
+
+let func7=(...arg)=>{return arg.length} //rest parametor
